319cc05d8d4373d69e629726bde56f33dc3273c7
DyaaNasrallh <dyaanasrallh@gmail.com>
2025-09-12 06:11:34 +0300

    count followers

---
+++ SampleFolder/followersCount/followers_count.js
@@ -0,0 +1,16 @@
+let count=0;
+function increaseCount(){
+    count++;
+    displayCount();
+    checkCountValue();
+}
+function displayCount(){
+document.getElementById("countDisplay").innerText=count;
+}
+function checkCountValue(){
+    if(count==10){
+        alert("Congratulations! You have reached 10 followers.");
+    }else if(count==20){
+        alert("Amazing! You have reached 20 followers.");
+    }
+}
